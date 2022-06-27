6531a72de3cedddf922bd86c4a781884cb519c4f
Albert Akrong <alphaxsalt@gmail.com>
2022-06-27 23:55:42 +0000

    feat: add grahpl functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,73 +4,43 @@ import {
   Flex,
   Heading,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Stack,
   Text,
   useToast,
 } from "@chakra-ui/react";
-import { DragEvent, useState } from "react";
+import { DragEvent, useMemo } from "react";
 import { RiAddCircleLine } from "react-icons/ri";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import AppWrapper from "./layout";
 import {
-  StatusQuery,
+  useCreateItemMutation,
   useCreateStatusMutation,
-  useItemsQuery,
+  useRemoveStatusMutation,
   useStatusesQuery,
 } from "./generated/graphql";
 import { ERROR_TOAST, SUCCESS_TOAST } from "./constants";
+import { GoTrashcan } from "react-icons/go";
 
-interface Todo {
-  id: number;
-  title: string;
-  sectionId: number;
-  userId: number;
-}
-interface TodoSection {
-  id: number;
-  title: string;
-  todos: Todo[];
-}
-
-const initialSections: TodoSection[] = [
-  {
-    id: 1,
-    title: "To do",
-    todos: [
-      { id: 1, title: "Todo 1", sectionId: 1, userId: 1 },
-      { id: 2, title: "Todo 2", sectionId: 1, userId: 2 },
-      { id: 3, title: "Todo 3", sectionId: 1, userId: 3 },
-    ],
-  },
-  {
-    id: 2,
-    title: "In Progress",
-    todos: [
-      {
-        id: 4,
-        title: "Lorem ipsum skdfajsdkfhajsd jkasdhv jbasdjbcka",
-        sectionId: 2,
-        userId: 1,
-      },
-      { id: 5, title: "Todo 5", sectionId: 2, userId: 2 },
-      { id: 6, title: "Todo 6", sectionId: 2, userId: 3 },
-    ],
-  },
-];
 function App() {
   const toast = useToast();
-  const { data: items, loading: itemsLoading } = useItemsQuery({
+  const {
+    data: statuses,
+    loading: statusesLoading,
+    refetch: refetchStatus,
+  } = useStatusesQuery({
     notifyOnNetworkStatusChange: true,
   });
 
-  const { data: statuses, loading: statusesLoading } = useStatusesQuery({
-    notifyOnNetworkStatusChange: true,
-  });
-
-  const [currentStatuses, setCurrentStatuses] = useState(statuses?.statuses);
+  const currentStatuses = useMemo(() => {
+    return statuses?.statuses;
+  }, [statuses]);
 
   console.log(currentStatuses);
-  const [createStatus, { loading }] = useCreateStatusMutation({
+  const [createStatus] = useCreateStatusMutation({
     onCompleted: () => {
       toast({ description: "Status created.", ...SUCCESS_TOAST });
     },
@@ -81,163 +51,221 @@ function App() {
       });
     },
   });
-  const [sections, setSection] = useState<TodoSection[]>(initialSections);
 
-  const addNewSection = () => {
-    const newSection: TodoSection = {
-      id: sections.length + 1,
+  const [createItem] = useCreateItemMutation({
+    onCompleted: () => {
+      toast({ description: "Item created.", ...SUCCESS_TOAST });
+    },
+    onError: () => {
+      toast({
+        description: "There was an error creating item.",
+        ...ERROR_TOAST,
+      });
+    },
+  });
+
+  const [removeStatus] = useRemoveStatusMutation({
+    onCompleted: () => {
+      toast({ description: "Status deleted.", ...SUCCESS_TOAST });
+    },
+    onError: () => {
+      toast({
+        description: "There was an error deleting status.",
+        ...ERROR_TOAST,
+      });
+    },
+  });
+
+  const addNewSection = async () => {
+    const newStatus = {
       title: "New Section",
       todos: [],
     };
-    setSection([...sections, newSection]);
+    console.log({ newStatus });
+    await createStatus({ variables: { createStatusInput: newStatus } });
   };
 
-  const addNewTodo = (sectionId: number) => {
-    const newTodo: Todo = {
-      id: sections.find((s) => s.id === sectionId)!.todos.length + 1,
+  const addNewTodo = async (statusId: string) => {
+    const newTodo = {
       title: "New Todo",
-      sectionId,
-      userId: 1,
+      status: statusId,
     };
-    const newSections = sections.map((s) => {
-      if (s.id === sectionId) {
-        return { ...s, todos: [...s.todos, newTodo] };
-      }
-      return s;
-    });
-    setSection(newSections);
+    await createItem({ variables: { createItemInput: newTodo } });
+    await refetchStatus();
+  };
+
+  const onDeleteStatus = async (statusId: string) => {
+    await removeStatus({ variables: { id: statusId } });
+    await refetchStatus();
   };
 
   const onDragStart = (
     e: DragEvent<HTMLDivElement>,
-    id: number,
-    sectionId: number
+    id: string,
+    statusId: string
   ) => {
     console.log("dragstart:", id);
-    e.dataTransfer.setData("id", id.toString());
-    e.dataTransfer.setData("currentSectionId", sectionId.toString());
+    e.dataTransfer.setData("id", id);
+    e.dataTransfer.setData("currentSectionId", statusId);
   };
 
   const onDragOver = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
 
-  const onDrop = (e: DragEvent<HTMLDivElement>, sectionId: number) => {
-    console.log("dropArea", sectionId);
+  const onDrop = (e: DragEvent<HTMLDivElement>, statusId: string) => {
+    console.log("dropArea", statusId);
     let id = e.dataTransfer.getData("id");
-    let currentSectionId = e.dataTransfer.getData("currentSectionId");
+    let currentStatusId = e.dataTransfer.getData("currentStatusId");
 
     //find todo
-    const todo = sections
-      .find((s) => s.id === +currentSectionId)!
-      .todos.find((t) => t.id === +id);
+    const item = currentStatuses
+      ?.find((s) => s.id === currentStatusId)!
+      .items?.find((t) => t.id === id);
 
-    if (!!todo) {
-      todo.sectionId = sectionId;
-      const newSections = sections.map((s) => {
-        if (s.id === +currentSectionId) {
-          const newTodos = s.todos.filter((t) => t.id !== todo.id);
-          return { ...s, todos: newTodos };
-        } else if (s.id === sectionId) {
-          const item = { ...todo };
-          item.sectionId = sectionId;
-          return { ...s, todos: [...s.todos, todo] };
+    if (!!item) {
+      item.status.id = statusId;
+
+      const newStatus = currentStatuses?.map((s) => {
+        if (s.id === currentStatusId) {
+          const newItems = s.items?.filter((t) => t.id !== item.id);
+          return { ...s, todos: newItems };
+        } else if (s.id === statusId) {
+          const _item = { ...item };
+          _item.status.id = statusId;
+          //@ts-ignore
+          return { ...s, todos: [...s.items, item] };
         }
         return s;
       });
-      setSection(newSections);
+      console.log({ newStatus });
+      // setSection(newStatus);
     }
   };
+
   return (
     <AppWrapper>
-      <Flex
-        direction="row"
-        overflowX="scroll"
-        overflowY="hidden"
-        whiteSpace="nowrap"
-        height="100vh"
-      >
-        {sections?.map?.((section) => (
-          <Box
-            sx={{
-              "&:not(:first-child)": {
-                ml: "30px",
-              },
-              borderRadius: "8px",
-            }}
-            display="inline-block"
-            key={section.id}
-            minW="300px"
-            maxW="300px"
-            onDrop={(e) => onDrop(e, section.id)}
-            onDragOver={onDragOver}
-          >
-            <Flex
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
+      {statusesLoading ? (
+        <Box>
+          <Text>Loading...</Text>
+        </Box>
+      ) : (
+        <Flex
+          direction="row"
+          overflowX="scroll"
+          overflowY="hidden"
+          whiteSpace="nowrap"
+          height="100vh"
+        >
+          {currentStatuses?.map?.((status) => (
+            <Box
+              sx={{
+                "&:not(:first-child)": {
+                  ml: "30px",
+                },
+                borderRadius: "8px",
+              }}
+              display="inline-block"
+              key={status.id}
+              minW="300px"
+              maxW="300px"
+              onDrop={(e) => onDrop(e, status.id)}
+              onDragOver={onDragOver}
             >
-              <Box>
-                <Heading as="h3" fontSize="2xl">
-                  {section.title}
-                </Heading>
-              </Box>
-              <Box>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <IconButton
-                    variant="ghost"
-                    p={0}
-                    aria-label="add-new-section"
-                    onClick={addNewSection}
-                  >
-                    <RiAddCircleLine />
-                  </IconButton>
-                  <Box>
-                    <IconButton variant="ghost" p={0} aria-label="more-options">
-                      <BiDotsHorizontalRounded />
-                    </IconButton>
-                  </Box>
-                </Stack>
-              </Box>
-            </Flex>
-            <Box mt="30px">
-              {section.todos?.map?.((todo) => (
-                <Box
-                  sx={{
-                    border: "1px solid",
-                    borderColor: "gray.400",
-                    "&:not(:first-child)": {
-                      mt: "10px",
-                    },
-                    borderRadius: "8px",
-                    "&:hover": {
-                      backgroundColor: "gray.100",
-                    },
-                  }}
-                  key={todo.id}
-                  padding={4}
-                  draggable
-                  onDragStart={(e) => onDragStart(e, todo.id, section.id)}
-                >
-                  <Text noOfLines={1}>{todo.title}</Text>
-                </Box>
-              ))}
-            </Box>
-            <Box mt="30px">
-              <Button
-                variant="dashed"
-                w="100%"
-                onClick={() => addNewTodo(section.id)}
+              <Flex
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
               >
-                <Stack spacing={2} direction="row" alignItems="center">
-                  <RiAddCircleLine />
-                  <Text>Add todo</Text>
-                </Stack>
-              </Button>
+                <Box>
+                  <Heading as="h3" fontSize="2xl">
+                    {status.title}
+                  </Heading>
+                </Box>
+                <Box>
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <IconButton
+                      variant="ghost"
+                      p={0}
+                      aria-label="add-new-status"
+                      onClick={addNewSection}
+                    >
+                      <RiAddCircleLine />
+                    </IconButton>
+                    <Box>
+                      <Menu>
+                        <MenuButton p={0} aria-label="more-options">
+                          <BiDotsHorizontalRounded />
+                        </MenuButton>
+                        <MenuList>
+                          <MenuItem
+                            onClick={() => onDeleteStatus(status.id)}
+                            icon={<GoTrashcan />}
+                            command="⌘T"
+                          >
+                            Delete
+                          </MenuItem>
+                        </MenuList>
+                      </Menu>
+                    </Box>
+                  </Stack>
+                </Box>
+              </Flex>
+              <Box mt="30px">
+                {status?.items?.map?.((item) => (
+                  <Flex
+                    direction="row"
+                    justifyContent="space-between"
+                    sx={{
+                      border: "1px solid",
+                      borderColor: "gray.400",
+                      "&:not(:first-child)": {
+                        mt: "10px",
+                      },
+                      borderRadius: "8px",
+                      "&:hover": {
+                        backgroundColor: "gray.100",
+                      },
+                    }}
+                    key={item.id}
+                    padding={4}
+                    draggable
+                    onDragStart={(e) => onDragStart(e, item?.id, status?.id)}
+                  >
+                    <Box>
+                      <Text noOfLines={1}>{item?.title}</Text>
+                    </Box>
+                    <Box>
+                      <Menu>
+                        <MenuButton p={0} aria-label="more-options">
+                          <BiDotsHorizontalRounded />
+                        </MenuButton>
+                        <MenuList>
+                          <MenuItem icon={<GoTrashcan />} command="⌘T">
+                            Delete
+                          </MenuItem>
+                        </MenuList>
+                      </Menu>
+                    </Box>
+                  </Flex>
+                ))}
+              </Box>
+              <Box mt="30px">
+                <Button
+                  variant="dashed"
+                  w="100%"
+                  onClick={() => addNewTodo(status?.id as string)}
+                >
+                  <Stack spacing={2} direction="row" alignItems="center">
+                    <RiAddCircleLine />
+                    <Text>Add todo</Text>
+                  </Stack>
+                </Button>
+              </Box>
             </Box>
-          </Box>
-        ))}
-      </Flex>
+          ))}
+        </Flex>
+      )}
     </AppWrapper>
   );
 }
---
+++ client/src/components/core/FillLoader.tsx
@@ -0,0 +1,9 @@
+const FillLoader = () => {
+  return (
+    <div className="fill-loader">
+      <div className="fill-loader__spinner">
+        <div className="fill-loader__spinner-inner" />
+      </div>
+    </div>
+  );
+};
